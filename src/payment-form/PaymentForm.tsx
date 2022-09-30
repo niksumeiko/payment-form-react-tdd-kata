a84d22e452864437e1f052bfa9a41f42aae6a84c
Nik Sumeiko <nik.sumeiko@gmail.com>
2022-09-30 19:00:48 +0200

    Handle form submit

--- src/payment-form/PaymentForm.tsx
+++ src/payment-form/PaymentForm.tsx
@@ -3,17 +3,38 @@ import type { FC } from 'react';
 import React from 'react';
 import { Button, Form, Input, InputNumber } from 'antd';
 
+interface FormValues {
+    iban: string;
+    amount: number;
+}
+
 export const PaymentForm: FC = () => {
+    const handleSubmit = (values: FormValues) => {
+        // eslint-disable-next-line no-console
+        console.log(values);
+    };
+
     return (
-        <Form layout="vertical" autoComplete="off">
-            <Form.Item label="Account number (IBAN)">
+        <Form layout="vertical" autoComplete="off" requiredMark={false} onFinish={handleSubmit}>
+            <Form.Item
+                label="Recipient (IBAN)"
+                name="iban"
+                rules={[{ required: true, message: 'Missing IBAN' }]}
+                data-test="iban"
+            >
                 <Input size="large" />
             </Form.Item>
-            <Form.Item label="Amount">
+            <Form.Item
+                label="Amount"
+                name="amount"
+                rules={[{ required: true, message: 'Missing payment amount' }]}
+                data-test="amount"
+            >
                 <InputNumber
                     style={{ width: '100%' }}
                     prefix="€"
                     addonAfter="EUR"
+                    placeholder="0,00"
                     controls={false}
                     min={0}
                     decimalSeparator="."
@@ -22,7 +43,7 @@ export const PaymentForm: FC = () => {
                 />
             </Form.Item>
             <Form.Item style={{ textAlign: 'center' }}>
-                <Button type="primary" htmlType="submit" size="large">
+                <Button type="primary" htmlType="submit" size="large" data-test="submit">
                     Pay
                 </Button>
             </Form.Item>
